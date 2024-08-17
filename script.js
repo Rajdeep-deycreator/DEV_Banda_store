9456e2ac0f6eb06918be5423f2edf797d9081a32
Rajdeep-deycreator <deyrajdeep569@gmail.com>
2024-08-17 20:43:02 +0530

    update

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-document.getElementById('d').addEventListener('click',function(){
-  window.location.href='/index.html'
+document.getElementById('').addEventListener('click',function(){
+  window.location.href="https://appsgeyser.io/18160362/Bhakti-Marg"
 })
-function h(){
-  alert("working");
-}
