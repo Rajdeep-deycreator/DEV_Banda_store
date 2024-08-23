99a0ac5aa66a326814f0f960b86545965fb98ef5
Rajdeep-deycreator <deyrajdeep569@gmail.com>
2024-08-23 06:25:48 +0530

    update

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('').addEventListener('click',function(){
-  window.location.href="https://appsgeyser.io/18160362/Bhakti-Marg"
+document.getElementById('d').addEventListener('click',function(){
+  window.location.href="https://www.mediafire.com/file/vak00agko1ftniu/_Bhakti_Marg_18160362_%25281%2529.apk/file"
 })
