84d568920ba425fc4350daed32b53d4ea5cc9674
Rajdeep-deycreator <deyrajdeep569@gmail.com>
2024-09-02 21:08:49 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('d').addEventListener('click',function(){
-  window.location.href="https://www.mediafire.com/file/vak00agko1ftniu/_Bhakti_Marg_18160362_%25281%2529.apk/file"
+document.getElementById('d').addEventListener('click',function () {
+  window.location.href="https://a2drive.com/1RgzRVrXzbpB/file";
 })
